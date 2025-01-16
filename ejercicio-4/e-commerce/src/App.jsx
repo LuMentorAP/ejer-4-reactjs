33884a2c74b872434065894b4f7096488c56ff35
LuMentorAP <157165812+LuMentorAP@users.noreply.github.com>
2025-01-16 14:13:41 -0300

    Add files via upload

--- ejercicio-4/e-commerce/src/App.jsx
+++ ejercicio-4/e-commerce/src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProductList from "./components/ProductList";
 import Cart from "./components/Cart";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Carousel from "./components/Carousel";
 
 function App() {
   // Estado para manejar el carrito
+  
   const [cart, setCart] = useState([]);
 
-  // Lista de productos disponibles
-  const products = [
+  // // Lista de productos disponibles
+  const productos = [
     { id: 1, name: "Laptop", price: 1000 },
     { id: 2, name: "Auriculares", price: 200 },
     { id: 3, name: "Teclado", price: 150 },
     { id: 4, name: "Mouse", price: 100 },
   ];
 
+ 
+  
   // Función para agregar un producto al carrito
   const addToCart = (product) => {
     setCart([...cart, product]);
@@ -25,12 +31,17 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Carrito de Compras</h1>
-      <ProductList products={products} addToCart={addToCart} />
+    <>
+      <Navbar />
+      <Carousel />
+      <ProductList productos={productos} addToCart={addToCart} />
       <Cart cart={cart} clearCart={clearCart} />
-    </div>
+      <Footer />
+    </>
   );
 }
 
 export default App;
+
+
+
--- ejercicio-4/e-commerce/src/components/ProductList.jsx
+++ ejercicio-4/e-commerce/src/components/ProductList.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 import "./../styles/ProductList.css";
 
-export default function ProductList({ products, addToCart }) {
+export default function ProductList({ productos, addToCart }) {
   return (
     <div className="product-list">
-      <h2>Productos Disponibles</h2>
+      <h2 className="text-center">Nuestros Productos</h2>
       <ul>
-        {products.map((product) => (
+        {productos.map((product) => (
           <li key={product.id} className="product-item">
             <span>{product.name} - ${product.price}</span>
             <button onClick={() => addToCart(product)}>Agregar al Carrito</button>
